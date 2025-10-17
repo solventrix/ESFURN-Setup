ded4180eedd0033d56c2f1646b525f0fc3713883
Peter Moorthamer <moorthamer@gmail.com>
2025-10-17 14:36:42 +0200

    Mount log folder to save the DQP log (JIRA AEKR-2301)

--- DataQualityPipeline/run-data-quality-pipeline.cmd
+++ DataQualityPipeline/run-data-quality-pipeline.cmd
@@ -6,10 +6,12 @@ SET IMAGE=data-quality-pipeline
 SET VERSION=1.10
 SET TAG=%VERSION%
 SET QA_FOLDER_HOST=%CD%/qa
+SET LOG_FOLDER_HOST=%CD%/logs
+SET LOG_FOLDER=/var/log/dqp
 
-echo "Docker login @ harbor.esfurn.org"
+echo "Docker login @ %REGISTRY%"
 docker login %REGISTRY%
 
 docker pull %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
 
-docker run --rm --name data-quality-pipeline --env REGISTRY=%REGISTRY% --env THERAPEUTIC_AREA=esfurn --env INDICATION=uc --env QA_FOLDER_HOST=%QA_FOLDER_HOST% --env SCRIPT_UUID=660d2280-3217-4bb3-8aab-c26da116e499 -v /var/run/docker.sock:/var/run/docker.sock --network feder8-net %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
+docker run --rm --name data-quality-pipeline --env REGISTRY=%REGISTRY% --env THERAPEUTIC_AREA=esfurn --env INDICATION=uc --env QA_FOLDER_HOST=%QA_FOLDER_HOST% --env LOG_FOLDER_HOST=%LOG_FOLDER_HOST% --env LOG_FOLDER=%LOG_FOLDER% --env SCRIPT_UUID=660d2280-3217-4bb3-8aab-c26da116e499 -v /var/run/docker.sock:/var/run/docker.sock -v %LOG_FOLDER_HOST%:%LOG_FOLDER% --network feder8-net %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
--- DataQualityPipeline/run-data-quality-pipeline.sh
+++ DataQualityPipeline/run-data-quality-pipeline.sh
@@ -7,17 +7,21 @@ IMAGE=data-quality-pipeline
 VERSION=1.10
 TAG=$VERSION
 
+LOG_FOLDER_HOST=${PWD}/logs
+LOG_FOLDER=/var/log/dqp
+QA_FOLDER_HOST=${PWD}/qa
+
 echo "Docker login @ $REGISTRY"
 docker login $REGISTRY
 docker pull $REGISTRY/$REPOSITORY/$IMAGE:$TAG
-
-QA_FOLDER_HOST=${PWD}/qa
 
 touch data-quality-pipeline.env
 echo "REGISTRY=$REGISTRY" >> data-quality-pipeline.env
 echo "THERAPEUTIC_AREA=esfurn" >> data-quality-pipeline.env
 echo "INDICATION=uc" >> data-quality-pipeline.env
 echo "QA_FOLDER_HOST=$QA_FOLDER_HOST" >> data-quality-pipeline.env
+echo "LOG_FOLDER_HOST=$LOG_FOLDER_HOST" >> data-quality-pipeline.env
+echo "LOG_FOLDER=$LOG_FOLDER" >> data-quality-pipeline.env
 echo "SCRIPT_UUID=660d2280-3217-4bb3-8aab-c26da116e499" >> data-quality-pipeline.env
 
 docker run \
@@ -25,6 +29,7 @@ docker run \
 --name data-quality-pipeline \
 --env-file data-quality-pipeline.env \
 -v /var/run/docker.sock:/var/run/docker.sock \
+-v $LOG_FOLDER_HOST:$LOG_FOLDER \
 --network feder8-net \
 $REGISTRY/$REPOSITORY/$IMAGE:$TAG
 
